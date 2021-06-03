2b000e83c87129508f063fcc5a784de517f73123
Mateus Marques <mateusmarquesdc@gmail.com>
2021-06-03 03:24:41 -0300

    Nova funcionalidade de criar artigos.

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../categories/Category')
+const slugify = require('slugify')
+const Article = require('./Article')
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send("Rota de aritgo")
 })
 
 router.get('/admin/articles/new', (req, res) => {
-    res.send("Rota para criar uma novo artigo")
+    Category.findAll().then( categories => {
+        res.render('admin/articles/new', {categories: categories})
+    })
+})
+
+router.post('/admin/articles/save', (req, res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect('/admin/articles')
+    })
 })
 
 module.exports = router
--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,8 @@
 const sequelize = require('sequelize')
 const connection = new sequelize('blog', 'root', '123456', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = connection
