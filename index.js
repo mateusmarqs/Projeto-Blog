ccb3a3123c70219cf455fb7744f5ee4d34848ac6
mateusmarqs <mateusmarquesdc@gmail.com>
2021-06-05 18:18:28 -0300

    Adicionando filtro por categorias de artigos.

--- index.js
+++ index.js
@@ -67,6 +67,27 @@ app.get('/:slug',(req,res) => {
     })
 })
 
+app.get('/category/:slug', (req, res) => {
+    var slug = req.params.slug
+
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}] //Incluindo todos os artigos dessa categoria
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render('index', {articles: category.articles, categories: categories})
+            })
+        } else {
+            res.redirect('/')
+        }
+    }).catch( err => {
+        res.redirect('/')
+    })
+})
+
 app.listen(8080, () => {
     console.log('O servidor está rodando')
 })
